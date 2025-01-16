51b1f72dc8bfb1ce7b067fb6a1494a7ba6404562
Abhi <ranjanabhi2468@gmail.com>
2025-01-16 23:17:57 +0530

    Fix last Update

--- app.js
+++ app.js
@@ -172,7 +172,7 @@ app.use((err,req,res,next)=>{
     }
     res.status(statusCode).render('error',{err});
 });
-
-app.listen(3000,()=>{
-    console.log("hello from 3000");
+const port=process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`Serving on port ${port}`);
 });

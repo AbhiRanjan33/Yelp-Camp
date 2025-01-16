eaf6da9253cf68845ad01fc93d9c4367a71eff4e
Abhi <ranjanabhi2468@gmail.com>
2025-01-17 00:16:06 +0530

    help pls

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if(process.env.NODE_ENV!=="production"){
     require('dotenv').config();
 };
 //require('dotenv').config();
-const mongoURI = process.env.MONGO_URI || 'mongodb://0.0.0.0:27017/yelp-camp';
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/yelp-camp';
 //console.log("Mongo_uri: ",mongoURI);
 
 const express=require('express');

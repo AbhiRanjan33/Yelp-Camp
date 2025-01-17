ef86a12bf6dfd7d209d8f45f25b064603bde2d0e
Abhi <ranjanabhi2468@gmail.com>
2025-01-17 15:23:28 +0530

    Finale

--- models/user.js
+++ models/user.js
@@ -1,15 +1,34 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-const passportLocalMongoose=require('passport-local-mongoose');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
 
-const UserSchema=new Schema({
-    email:{
-        type:String,
-        required:true,
-        unique:true
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
     }
 });
 
-UserSchema.plugin(passportLocalMongoose);
+// Hash the password before saving the user
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
 
-module.exports=mongoose.model('User',UserSchema);
+// Method to compare passwords
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
--- schemas.js
+++ schemas.js
@@ -1,43 +1,41 @@
-const BaseJoi=require('joi');
-const sanitizeHtml=require('sanitize-html');
+const BaseJoi = require('joi');
+const DOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
 const { validate } = require('./models/user');
 
-const extension=(joi)=>({
-    type:'string',
-    base:joi.string(),
-    messages:{
-        'string.escapeHTML':'{{#label}} must not include HTML!'
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
     },
-    rules:{
-        escapeHTML:{
-            validate(value,helpers){
-                const clean=sanitizeHtml(value,{
-                    allowedTags:[],
-                    allowedAttributes:{},
-                });
-                if(clean!==value) return helpers.error('string.escapeHTML',{value})
-                return clean;    
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const window = new JSDOM('').window;
+                const clean = DOMPurify(window).sanitize(value);
+                if (clean !== value) return helpers.error('string.escapeHTML', { value });
+                return clean;
             }
         }
     }
 });
 
-const Joi=BaseJoi.extend(extension)
+const Joi = BaseJoi.extend(extension);
 
-module.exports.campgroundSchema=Joi.object({
-    campground:Joi.object({
-        title:Joi.string().required().escapeHTML(),
-        price:Joi.number().required().min(0),
-        //image:Joi.string().required(),
-        location:Joi.string().required().escapeHTML(),
-        description:Joi.string().required().escapeHTML(),
+module.exports.campgroundSchema = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required().escapeHTML(),
+        price: Joi.number().required().min(0),
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML(),
     }).required(),
-    deleteImages:Joi.array()
+    deleteImages: Joi.array()
 });
 
-module.exports.reviewSchema=Joi.object({
-    review:Joi.object({
-        rating:Joi.number().required().min(1).max(5),
-        body:Joi.string().required().escapeHTML()
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required().escapeHTML()
     }).required()
 });
